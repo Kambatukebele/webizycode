a3913f3ed614e62459739e6814dbc114f7128f3a
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-09-01 17:42:58 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,17 +23,47 @@
       href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
       rel="stylesheet">
     <!-- End Google Fonts -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Vite -->
     @vite('resources/css/app.css')
     <title>Document</title>
   </head>
 
   <body>
+    <!--Hidden Navigation and logo for Small devices -->
+    <nav class="bg-white w-full lg:hidden">
+      <div class="flex justify-between items-center py-2 w-[90%] mx-auto sm:max-w-[600px] md:max-w-[700px] ">
+        <!--Logo -->
+        <div class="block">
+          <a href="#" class="">
+            <img class="w-[30px]" src="/assets/images/originale-logo-webizy-1.png" alt="webizy-code-logo">
+          </a>
+        </div>
+        <!--Hamburger Menu-->
+        <div class="block cursor-pointer">
+          <i class="fa-solid fa-bars block text-xl"></i>
+        </div>
+        <!-- Close Menu -->
+        <div class="hidden">
+          <i class="fa-solid fa-xmark text-xl"></i>
+        </div>
+        <!-- Menu -->
+        <ul class="hidden">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!--ENd Hidden Navigation and logo for Small devices -->
     <!-- Header -->
-    <header class="w-full h-full bg-[url('/public/assets/images/bg-image.png')] bg-cover bg-center bg-no-repeat bg-">
-      <div class="">
+    <header class="w-full h-full bg-[url('/public/assets/images/bg-image.png')] bg-cover bg-center bg-no-repeat sm:h-[75vh] sm:relative lg:h-screen">
+      <div class="w-[90%] mx-auto sm:max-w-[600px] md:max-w-[700px] lg:max-w-[900px] xl:max-w-[1220px]">
         <!-- Navigation and logo -->
-        <nav class="">
+        <nav class="hidden lg:flex lg:justify-between lg:items-center lg:w-full">
           <!-- Logo -->
           <div class="">
             <a href="#" class="">
@@ -41,40 +71,54 @@
             </a>
           </div>
           <!-- Navigation Menu-->
-          <ul class="">
-            <li class=""><a class="" href="/">Home</a></li>
-            <li class=""><a class="" href="/">Services</a></li>
-            <li class=""><a class="" href="/">Home</a></li>
-            <li class=""><a class="" href="/">Home</a></li>
-            <li class=""><a class="" href="/">Home</a></li>
-            <li class=""><a class="" href="/">Home</a></li>
+          <ul class="lg:flex lg:justify-between lg:items-center lg:w-[400px]">
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Home</a></li>
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Services</a></li>
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Home</a></li>
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Home</a></li>
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Home</a></li>
+            <li class="lg:text-white lg:text-base lg:font-normal"><a class="" href="/">Home</a></li>
           </ul>
           <!-- CTA -->
-          <div class="">
-            <a href="$">Free Consultancy</a>
+          <div class="block">
+            <a class="block text-center text-base font-normal text-purple-900 bg-white w-52 mx-auto py-3 my-5 rounded-3xl uppercase" href="$">Free Consultancy</a>
           </div>
         </nav>
-        <!-- Hero section -->
-        <div class="">
+        <!-- end Navigation and logo -->
+        <!-- Hero section  -->
+        <div class="block text-center sm:flex sm:justify-between sm:items-center  sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:w-[600px] sm:h-[500px] md:w-[700px] lg:w-[900px] xl:w-[1220px] lg:mt-10">
           <!-- Hero text -->
-          <div class="">
-            <small class="">Boot Your Business</small>
-            <h1 class="">Secure Solutions for Digital Assets & Money Easily</h1>
-            <p class="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, laborum!
-            </p>
-            <a class="" href="#">
-              Learn More
-            </a>
+          <div class=" block py-1 sm:flex sm:flex-col sm:justify-start sm:items-start sm:h-full  sm:w-[260px] md:w-[450px] xl:w-[600px]">
+            <small class="text-white font-Nunito font-normal text-xs block my-3 ">Boot Your Business</small>
+            <div class="mb-4 w-[300px] sm:mx-0 sm:w-full md:w-[350px] lg:w-[450px] xl:w-[600px]">
+              <h1 class="text-white font-bold text-xl font-ReadexPro sm:text-left sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl xl:leading-16xl:max-w-[500px]">Enthusiastic about coding. Fueled by the
+                power of Laravel.
+              </h1>
+            </div>
+            <div class="mb-4  max-w-[300px] mx-auto sm:mx-0 sm:max-w-[250px] md:max-w-[350px] lg:max-w-[450px]">
+              <p class="text-white text-center font-normal text-xs font-NunitoSans sm:text-sm sm:text-left">
+                Since 2020, we've been crafting, overseeing, and enhancing WordPress and Laravel applications 
+              </p>
+            </div>
+            <div class="block ">
+              <a class="block text-center text-base font-normal text-purple-900 bg-white w-44 mx-auto py-3 my-5 rounded-3xl uppercase"
+                href="#">
+                Learn More
+              </a>
+            </div>
           </div>
           <!-- Hero Image -->
-          <div class="">
-            <img class="" src="/assets/images/slider-girl-img-1.png" alt="slider-girl-image">
+          <div class="block w-full sm:w-[380px] sm:h-[380px] sm:absolute top-20 right-0 sm:flex sm:justify-center sm:items-center md:w-[410px] md:h-[410px] lg:w-[510px] lg:h-[510px] xl:w-[710px] xl:h-[710px]">
+            <img class="w-full h-full object-cover object-center" src="/assets/images/slider-girl-img-1.png"
+              alt="slider-girl-image">
           </div>
         </div>
       </div>
-
     </header>
+    <!-- Hidden Header for Small devices -->
+    {{-- <header>
+
+    </header> --}}
   </body>
   <script src="/assets/js/index.js"></script>
 
